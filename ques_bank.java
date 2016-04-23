8242435a5513d6fe8f212b734843b04779cd0a38
Donovan M. Vaz <donovan.mv@gmail.com>
2016-04-23 23:30:46 +0530

    Added code for generating data text file

---
+++ ques_bank.java
@@ -0,0 +1,74 @@
+import java.io.*;
+
+public class ques_bank
+{
+     public static void main(String[] args)throws IOException
+     {
+         write();
+     }
+     public static void write()throws IOException
+     {
+         PrintWriter pw=new PrintWriter(new FileWriter("Question bank.kbc",false));
+         pw.println("01*Large breed of cat, North America's only native long-haired breed*A)Maine Coon Cat_B)Siameese_C)Puma_D)Himalayan_*A)Maine Coon Cat");
+         pw.println("02*Common name applied to many plants of the lugume family*A)Fir_B)Ivy_C)Bean_D)Lily_*C)Bean");
+         pw.println("03*Breed of hunting dog that is the oldest known of the type of hound that hunts by scent alone*A)Bloodhound_B)Dachshund_C)Bouvier des Flandres_D)German Sheperd_*A)Bloodhound");
+         pw.println("04*Traditional titles used by monarchs in Islamic countries*A)Emperor_B)King_C)Sultan_D)Ayatollah_*C)Sultan");
+         pw.println("05*The Indian who recently became the world's richest man*A)Anil Ambani_B)Mukesh Ambani_C)Dirubai Ambani_D)Yuvraj Singh_*B)Mukesh Ambani");
+         pw.println("06*Surface or structure upon which a relegious sacrifice is offered*A)Basilica_B)Alter_C)Crown_D)Reliquary_*B)Alter");
+         pw.println("07*The ultimate aim of the Buddhist relegous path*A)Transmigration_B)Nirvana_C)Dharma_D)Mitosis_*B)Nirvana");
+         pw.println("08*In law, succession to the property of a deceased indivisual*A)Inheritance_B)Transmission_C)Will_D)Mortgage_*A)Inheritance");
+         pw.println("09*Practice by which powerfull nations extend and mantain control of weaker nations*A)Imperialism_B)Democracy_C)Liberalism_D)Facism_*A)Imperialism");
+         pw.println("10*Branch of Science that studies poisons*A)Psychopharmacology_B)Thermography_C)Toxicology_D)Pathology_*C)Toxicology");
+         pw.println("11*Vatican city is located in an enclave within this city*A)Paris_B)Rome_C)London_D)Berlin_*B)Rome");
+         pw.println("12*High-pitched, horizontaly held flute, historically associated with ifantry troops*A)Bagpipe_B)Fife_C)Accordian_D)Trumpet_*B)Fife");
+         pw.println("13*Sometimes reffered to by the single term numismatics*A)Book collecting_B)Stamp collecting_C)Medal collecting_D)Coin collecting_*D)Coin collecting");
+         pw.println("14*Great mountain system of eastern North America, nearly parallel with the Atlantic coast*A)Continental Divide_B)Appalachian Mountains_C)Dolomites_D)Cambrian Mountains_*B)Appalachian Mountains");
+         pw.println("15*Power or ability of the human mind to choose a course of action*A)Hedonism_B)Logic_C)Intuition_D)Free will_*D)Free will");
+         pw.println("16*What is the name of the condition in which a person experiences episodes of depression that typically begin each fall of winter and lift in the spring?*A)Delirium_B)Seasonal Affective Disorder_C)Obssesive-Compulsive Disorder_D)Anorexia Nervosa_*B)Seasonal Affective Disorder");
+         pw.println("17*This group of arthropods is known for having many legs*A)Barnacle_B)Larva_C)Millipede_D)Worm_*C)Millipede");
+         pw.println("18*In Geology, this is the study of rock layers*A)Paleography_B)Stratigraphy_C)Demography_D)Crystallography_*B)Stratigraphy");
+         pw.println("19*Cured forage plants used as feed for livestock*A)Rice_B)Legume_C)Palms_D)Hay_*D)Hay");
+         pw.println("20*The auther of King Lear*A)Jhonson,Ben_B)Shakespeare,William_C)Racine,Jean Baptiste_D)Moliere_*B)Shakespeare, William");
+         pw.println("21*Fast-paced game played on a rectangular court, genarally indoors, by 2 teams of 5 players each*A)Golf_B)Basketball_C)Squash_D)Lacrosse_*B)Basketball");
+         pw.println("22*Fortified residence of a fuedal lord or monarch*A)Taj Mahal_B)Castle_C)Obelisk_D)Pagoda_*B)Castle");
+         pw.println("23*Relegious beliefs of the people of ancient Mesopotamia*A)Eastern Rite Church_B)Sumerian religion_C)Hinduism_D)Judaism_*B)Sumerian religion");
+         pw.println("24*Capital of Canada*A)Ontario_B)Quebec_C)Ottawa_D)Montreal_*C)Ottawa");
+         pw.println("25*In Music, sound that has a definite pitch*A)Tone_B)Hymn_C)Score_D)Note_*A)Tone");
+         pw.println("26*An aristocratic or patrician class*A)Heretics_B)Nobility_C)Bourgeoisie_D)Peasants_*B)Nobility");
+         pw.println("27*This type of canyon occurs in desert areas and is produced by water runoff*A)Gully_B)Atoll_C)Arroyo_D)Plateau_*C)Arroyo");
+         pw.println("28*Which trees are part of the rose family*A)Apple_B)Prickly Ash_C)Coconut_D)Acacia_*A)Apple");
+         pw.println("29*In Greek mythology, the queen of the Gods who was also the sister and wife of Zeus*A)Hercules_B)Freya_C)Hera_D)Kali_*C)Hera");
+         pw.println("30*Cleric attached to a household or a public institution*A)Chaplain_B)Dominicans_C)Scholar_D)Deacon_*A)Chaplain");
+         pw.println("31*Popular family card game that originated in Italy im the 19th century*A)Checkers_B)Casino_C)Poker_D)Black-Jack_*B)Casino");
+         pw.println("32*Hawaiian musical instrument resembling a small guiter*A)Accordion_B)Gong_C)Didjeridu_D)Ukulele_*D)Ukulele");
+         pw.println("33*The particles of light are called...*A)Gluons_B)Atoms_C)Photons_D)Quarks_*C)Photons");
+         pw.println("34*More than 50% of the stars in the sky are members of two-star systems called...*A)Constellations_B)Galaxies_C)Black Holes_D)Binaries_*D)Binaries");
+         pw.println("35*Anglo-American pirate, known as captain Kid*A)Blackbeard_B)Jones, John Paul_C)Billy the Kid_C)Kid, William_*C)Kid, William");
+         pw.println("36*The family of water birds which also include ducks and swans*A)Jay_B)Loon_C)Goose_D)Grouse_*C)Goose");
+         pw.println("37*Largest and lowest-pithed instrument of the violin family*A)Double bass_B)Viol_C)Viola_D)Cello_*A)Double bass");
+         pw.println("38*The vital endocrine gland located on top of each kidney*A)Adrenal gland_B)Pitutary gland_C)Thyroid gland_D)Liver_*A)Adrenal gland");
+         pw.println("39*Inland sea between south-eastern Europe and Asia minor*A)North Sea_B)Mediterranean Sea_C)Sea of Japan_D)Black Sea_*D)Black Sea");
+         pw.println("40*The waterway which divides Europe from Asia and on either side of which lies Istanbull*A)The Strait of Malacca_B)The Bosporus_C)Suez Canal_D)The Dardanalles_*B)The Bosporus");
+         pw.println("41*Spanish seaport on the Mediterranean Sea*A)Barcelona_B)Madrid_C)Pamplona_D)Catalonia_*A)Barcelona");
+         pw.println("42*Historic province of Ireland and another name for Northern Ireland*A)Yokshire_B)Ulster_C)Cornwall_D)Istle of Wight_*B)Ulster");
+         pw.println("43*The return of Jesus Christ in visible form to Earth*A)Revelation_B)Second coming_C)Vom Kippur_D)Transfiguration_*B)Second coming");
+         pw.println("44*An artwork made by pasting together heterogeneous materials*A)Trompe I'Oeil_B)Cubism_C)Collage_D)Stencil_*C)Collage");
+         pw.println("45*How is Albinism inherited in humans?*A)Domonant trait_B)Sex-linked trait_C)Sex-influenced trait_D)Recessive trait_*D)Recessive trait");
+         pw.println("46*Who established the world's first movie studio?*A)Bogart,Humphrey_B)Edison,Thomas Alva_C)Warner Brothers_D)Chaplin,Charlie_*B)Edison,Thomas Alva");
+         pw.println("47*American physicist and government advisor who directed the developement of the first atomic bombs*A)Neumann,John von_B)Teller,Edward_C)Oppenheimer,J.Robert_D)Fermi,Enrico_*C)Oppenheimer,J.Robert");
+         pw.println("48*Founder of Islam*A)James Blunt_B)Khruschev_C)Zakir Hussain_D)Prophet Muhammed_*D)Prophet Muhammed");
+         pw.println("49*Largest country of South America*A)Brazil_B)Honduras_C)Costa Rica_D)Chile_*A)Brazil");
+         pw.println("50*A hollow chamber below the surface of the earth*A)Canyon_B)Valley_C)Cave_D)Glacier_*C)Cave");
+         pw.println("51*The sun belongs to this galaxy*A)Local group_B)Solar system_C)Milky Way_D)Andromeda_*C)Milky Way");
+         pw.println("52*Which fuel is most commonly used for the smelting of iron?*A)Coal_B)Coke_C)Petroleum_D)Nuclear Energy_*B)Coke");
+         pw.println("53*Art and science involved in the creation of machine-made products*A)Industrial design_B)Landscape Architecture_C)Art Conservation_D)Interior design_*A)Industrial design");
+         pw.println("54*Russian chess player who was born in Saint Petersburg*A)Edmond Hoyle_B)Bobby Fischer_C)Vishwanathan Anand_D)Mikhail Botvinnik_*D)Mikhail Botvinnik");
+         pw.println("55*The Greek god Dionysus had female devotees known as...*A)Eunuchs_B)Maenads_C)Amazons_D)Valkyries_*B)Maenads");
+         pw.println("56*These animals were the first vertebrates to evolve as land dwellers*A)Amphibians_B)Mamals_C)Birds_D)Reptiles_*A)Amphibians");
+         pw.println("57*Capital of Florida*A)New Jersy_B)New York_C)Miami_D)Tallahassee_*D)Tallahassee");
+         pw.println("58*The peace treaty which ended World War 1 is known as...*A)The Warsaw Pact_B)The Geneva Convention_C)The Treaty of Versailles_D)The Maastricht Treaty_*C)The Treaty of Versailles");
+         pw.println("59*Name of the highest class in Hinduism*A)Brahmin_B)Shudra_C)Kshatriya_D)Vaishnava_*A)Brahmin");
+         pw.println("60*In Roman mythology, beautiful princess loved by Cupid, God of love*A)Flora_B)Venus_C)Neptune_D)Psyche_*D)Psyche");
+         pw.close();
+     }
+}
